5b92b4472ec8f811db3c966f98937143c165d51e
Christian Briones <cwbriones@gmail.com>
2013-08-23 15:16:53 -0700

    Made API similar to std::unordered_map. Overloaded []

--- hashmap/HashMap.h
+++ hashmap/HashMap.h
@@ -10,8 +10,6 @@
 // Useful struct for iteration, mirrors std::pair
 template <class Key, class Value>
 struct Pair {
-    Pair(const Key& key, const Value& value) :
-        first(key), second(value) {}
     Key first;
     Value second;
 };
@@ -22,7 +20,9 @@ Pair<Key, Value> create_pair(const Key& key, const Value& value){
     return Pair<Key, Value>(key, value);
 }
 
-// Hash Table implemented with linear probing
+/**
+ * Hash Table container class implemented with linear probing
+ */
 template <typename Key, typename Value>
 class HashMap {
 public:
@@ -30,7 +30,7 @@ public:
     ~HashMap();
 
     void put(const Key& key, const Value& value);
-    Value* get(const Key& key);
+    Value& get(const Key& key);
     void remove(const Key& key);
 
     bool contains(const Key& key) const;
@@ -40,6 +40,9 @@ public:
     }
     void set_max_load(float maxLoad){
         maxLoad_ = maxLoad;
+    }
+    Value& operator[](const Key& key){
+        return get(key);
     }
 
     class Iterator;
@@ -66,6 +69,9 @@ HashMap<Key, Value>::~HashMap(){
     }
 }
 
+/**
+ * Inserts the key-value pair into the HashMap.
+ */
 template <class Key, class Value>
 void HashMap<Key, Value>::put(const Key& key, const Value& value){
     // Double the size of the linear-probing table if
@@ -84,20 +90,35 @@ void HashMap<Key, Value>::put(const Key& key, const Value& value){
         }
     }
     // Otherwise empty slot
-    items_[i] = new Pair<Key, Value>(key, value);
+    items_[i] = new Pair<Key, Value>();
+    items_[i]->first = key;
+    items_[i]->second = value;
     size_++;
 }
 
+/** 
+ * Returns a reference to the value in the HashMap at key.
+ * Creates a new pair if not in the map.
+ */
 template <class Key, class Value>
-Value* HashMap<Key, Value>::get(const Key& key){
-    for(int i = hashcode(key) % tableSize_; items_[i]; i = (i + 1) % tableSize_){
+Value& HashMap<Key, Value>::get(const Key& key){
+    int i = hashcode(key) % tableSize_;
+    for(; items_[i]; i = (i + 1) % tableSize_){
         if (items_[i]->first == key){
-            return &items_[i]->second;
+            return items_[i]->second;
         }
     }
-    return nullptr;
+    // Create a new key-value pair and return reference
+    // to the value.
+    items_[i] = new Pair<Key, Value>();
+    items_[i]->first = key;
+
+    return items_[i]->second;
 }
 
+/**
+ * Returns true if the key is contained in the HashMap.
+ */
 template <class Key, class Value>
 bool HashMap<Key, Value>::contains(const Key& key) const {
     for(int i = hashcode(key) % tableSize_; items_[i]; i = (i + 1) % tableSize_){
@@ -108,29 +129,33 @@ bool HashMap<Key, Value>::contains(const Key& key) const {
     return false;
 }
 
+/** 
+ * Resizes the HashMap and rehashes all of the key-value pairs into new
+ * locations.
+ */
 template <class Key, class Value>
 void HashMap<Key, Value>::resize(int newSize){
-    // std::vector<Key*> old_keys = keys_;
-    // std::vector<Value*> old_vals = values_;
+    std::vector<Pair<Key, Value>*> old = items_;
 
-    // // Reset
-    // keys_.clear();
-    // values_.clear();
-    // keys_.resize(newSize);
-    // values_.resize(newSize);
-    // for (int i = 0; i < newSize; i++){
-    //     keys_[i] = nullptr;
-    //     values_[i] = nullptr;
-    // }
+    // Reset
+    items_.clear();
+    items_.resize(newSize);
+    for (int i = 0; i < newSize; i++){
+        items_[i] = nullptr;
+    }
 
-    // // Rehash
-    // // TODO: this is a memory leak, the values in old_keys and
-    // // old_vals are never freed
-    // for (int i = 0; i < newSize; i++){
-    //     if (old_keys[i]){
-    //         put( *old_keys[i], *old_vals[i] );
-    //     }
-    // }
+    // Rehash
+    for (int i = 0; i < tableSize_; i++){
+        if (old[i]){
+            int i = hashcode(old[i]->first) % tableSize_;
+            while(items_[i]){
+                i = (i + 1) % tableSize_;
+            }
+            items_[i] = old[i];
+        }
+    }
+
+    tableSize_ = newSize;
 };
 
 template <class Key, class Value>
